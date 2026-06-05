e796cfd0dd713f469af21736a242cabc3230338c
dungeoncake <140109783+dungeoncake@users.noreply.github.com>
2026-06-05 18:16:41 -0400

    Refactor modal functions and collapsible behavior
    Updated modal display style to 'flex' and improved event listener functionality.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
@@ -13,27 +13,24 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-// Open the Modal
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
-  document.getElementById("myModal").classList.add("active");
+  var modal = document.getElementById("myModal");
+  modal.style.display = "flex";
+  modal.classList.add("active");
 }
 
-// Close the Modal
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-  document.getElementById("myModal").classList.remove("active");
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+  modal.classList.remove("active");
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -41,10 +38,10 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
